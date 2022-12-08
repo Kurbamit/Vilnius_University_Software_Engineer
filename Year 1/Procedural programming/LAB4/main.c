04d8ff37ed41ea1f59227b538da56e7574885b47
Dominykas Černovas <113939016+Kurbamit@users.noreply.github.com>
2022-12-08 18:17:05 +0200

    Update main.c

--- Year 1/Procedural programming/LAB4/main.c
+++ Year 1/Procedural programming/LAB4/main.c
@@ -1,162 +1,35 @@
+//Dominykas Černovas
+//Programų sistemos  1 kursas 3 grupė
+//Ketvirtoji užduotis
+//Sudaryti dvipusį sąrašą,
+//Parašyti procedūrą, kuri ištrina mažiausią
+//Elementą
 #include <stdio.h>
-#include <stdlib.h>
+#include "file.h"
 
-typedef struct node{
-    struct node *prev;
-    int number;
-    struct node *next;
-} node;
-
-node *createList(struct node *head, int n);
-node *addToEmpty(node *head, int data);
-node *addAtEnd(node *head, int data);
-node *deleteSmallest(node *head, int n);
-void cleanUp(node *head);
 
 int main()
 {
     node *head = NULL;
-    node *ptr;
-    int n;
+    int numberOfItems = 4;
 
-    printf("Enter number of the nodes: ");
-    scanf("%d", &n);
+    //Creating list
+    AddToEmpty(&head, 3);
+    Push(&head, 2);
+    Push(&head, 4);
+    Push(&head, 5);
 
-    head = createList(head, n);
-    printf("\nN: %d\n\n", n);
+    PrintList(head);
 
-    ptr = head;
-    while(ptr != NULL)
-    {
-        printf("NUMBER:%d ADDRESS:%p PREVADD:%p NEXTADD:%p\n", ptr->number, ptr, ptr->prev, ptr->next);
-        ptr = ptr->next;
-    }
-    head = deleteSmallest(head, n);
+    //Deleting the smallest element from the list
+    DeleteSmallest(&head, numberOfItems);
+
     printf("\n\n");
-    ptr = head;
-    while(ptr != NULL)
-    {
-        printf("NUMBER:%d ADDRESS:%p PREVADD:%p NEXTADD:%p\n", ptr->number, ptr, ptr->prev, ptr->next);
-        ptr = ptr->next;
-    }
+    printf("Deleted smallest element: \n");
+    PrintList(head);
 
-    cleanUp(head);
+    // Free all the pointers
+    CleanUp(head);
 
     return 0;
-}
-
-node *createList(struct node *head, int n)
-{
-    int data;
-
-    if(n <= 0)
-        return head;
-
-    printf("Enter number of the 1 node: ");
-    scanf("%d", &data);
-
-    head = addToEmpty(head, data);
-
-    for(int i = 1; i < n; ++i)
-    {
-        printf("Enter number of the %d node: ", i + 1);
-        scanf("%d", &data);
-        head = addAtEnd(head, data);
-    }
-    return head;
-}
-
-node *addToEmpty(node *head, int data)
-{
-    node *temp = malloc(sizeof(node));
-    temp->prev = NULL;
-    temp->next = NULL;
-    temp->number = data;
-    head = temp;
-
-    return head;
-}
-
-node *addAtEnd(node *head, int data)
-{
-    node *temp, *tp;
-
-    temp = malloc(sizeof(node));
-    temp->prev = NULL;
-    temp->next = NULL;
-    temp->number = data;
-    tp = head;
-
-    while (tp->next != NULL)
-        tp = tp->next;
-
-    tp->next = temp;
-    temp->prev = tp;
-
-    return head;
-}
-
-node *deleteSmallest(node *head, int n)
-{
-    node *smallest, *temp, *temporaryHead;
-    int position = 1;
-    temporaryHead = head;
-    smallest = head;
-
-    // Which one is the smallest one
-    for(int i = 1; temporaryHead != NULL; ++i)
-    {
-        temp = temporaryHead->next;
-        if(smallest->number > temporaryHead->number)
-        {
-            smallest = temporaryHead;
-            position = i;
-        }
-        temporaryHead = temp;
-    }
-
-    temporaryHead = NULL;
-
-    temp = head;
-
-    // IF node is in the middle
-    if(position > 1 && position < n)
-    {
-        while (position > 1) {
-            temp = temp->next;
-            --position;
-        }
-        temporaryHead = temp->prev;
-        temporaryHead->next = temp->next;
-        temp->next->prev = temporaryHead;
-        free(temp);
-        temp = NULL;
-    }else if(position == 1)             // If node is the first element
-    {
-        head = head->next;
-        free(head->prev);
-        head->prev = NULL;
-    }else                               // If node is the last element
-    {
-        while(temp->next != NULL)
-            temp = temp->next;
-        temporaryHead = temp->prev;
-        temporaryHead->next = NULL;
-        free(temp);
-        temp = NULL;
-    }
-
-    return head;
-}
-
-void cleanUp(node *head)
-{
-    node *next;
-
-    while(head != NULL)
-    {
-        next = head->next;
-        free(head);
-        head = next;
-    }
 }
