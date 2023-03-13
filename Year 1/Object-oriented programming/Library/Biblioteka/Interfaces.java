22e48fb90ba0be0a69595169e97b9ad06293851f
Dominykas Černovas <113939016+Kurbamit@users.noreply.github.com>
2023-03-13 17:24:32 +0200

    Create Interfaces.java

---
+++ Year 1/Object-oriented programming/Library/Biblioteka/Interfaces.java
@@ -0,0 +1,17 @@
+package Biblioteka;
+
+interface BibliotekaLeidinys{
+    boolean isduoti();
+    boolean grazinti();
+}
+
+interface IsduotiKnyga extends BibliotekaLeidinys{
+    int MAX_BOOK_ISSUED_DAYS = 30;
+    boolean pratestiKnyga(int days);
+    boolean pratestiKnyga();
+}
+
+interface IsduotiZurnala extends BibliotekaLeidinys{
+    int MAX_JOURNAL_ISSUED_DAYS = 1;
+    boolean isduoti();
+}
