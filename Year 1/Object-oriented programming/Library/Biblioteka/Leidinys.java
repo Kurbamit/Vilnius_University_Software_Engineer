6759f52d821aeef1c21db1cb2fd302210f01caa4
Dominykas Černovas <113939016+Kurbamit@users.noreply.github.com>
2023-03-13 17:23:58 +0200

    Update Leidinys.java

--- Year 1/Object-oriented programming/Library/Biblioteka/Leidinys.java
+++ Year 1/Object-oriented programming/Library/Biblioteka/Leidinys.java
@@ -1,99 +1,75 @@
 package Biblioteka;
 import java.time.LocalDate;
 
-public class Leidinys {
+public abstract class Leidinys implements BibliotekaLeidinys{
     // Fields (laukai)
     private String name;
-    private String author;
+    private int issueYear;
     private LocalDate issueDate;
     private LocalDate returnDate;
     private boolean issued = false;
-
-    protected final int MAX_BOOK_ISSUED_DAYS = 30;
-    protected final int MAX_JOURNAL_ISSUED_DAYS = 1;
     private static int publicationCount = 0;
+
 
     // Constructor
     public Leidinys(){
-        this(null, null);
+        this(null,  0);
     };
-    public Leidinys(String name, String author) {
+    public Leidinys(String name, int issueYear) {
         this.name = name;
-        this.author = author;
+        this.issueYear = issueYear;
         ++publicationCount;
     }
 
-    // Methods
-    public void println(String type, int issueYear){
-        System.out.println("Autorius:      " + author);
-        System.out.println("Pavadinimas:   " + name);
-        System.out.println("Išduota:       " + issueDate);
-        System.out.println("Grąžinti:      " + returnDate);
-        System.out.println("Tipas:         " + type);
-        System.out.println("Leidimo metai: " + issueYear);
+    public String toString(){
+        return "\nPavadinimas: " + name + "\nLeidimo metai: " + issueYear + "\nIšduota: " +
+                issueDate + "\nGrąžinti: " + returnDate;
     }
+
+    // Methods
 
     //  !!Static method can only access static fields!!
     public static int isVisoLeidiniu(){
         return publicationCount;
     }
 
-    public boolean isduoti(int days)
+    public boolean isduoti()
     {
         if(this.issued)
             return false;
         else{
             this.issued = true;
             this.issueDate = LocalDate.now();
-            this.returnDate = LocalDate.now().plusDays(days);
+            this.returnDate = LocalDate.now().plusDays(7);
             return true;
         }
     }
-//    public boolean isduoti()
-//    {
-//        if(this.issued)
-//            return false;
-//        else if(this.type.equals("Knyga")){
-//            this.issued = true;
-//            this.issueDate = LocalDate.now();
-//            this.returnDate = LocalDate.now().plusDays(MAX_ISSUED_DAYS);
-//            return true;
-//        }else
-//        {
-//            this.issued = true;
-//            this.issueDate = LocalDate.now();
-//            this.returnDate = LocalDate.now().plusDays(1);
-//            return true;
-//        }
-//    }
-    // * Methods that can not be overridden
-    public final boolean pratestiLeidini(int days){
-        if(!this.issued) {
-            return false;
-        }else {
-            returnDate = returnDate.plusDays(days);
+
+    // Methods that can not be overridden
+    public final boolean grazinti(){
+        if(this.issued)
+        {
+            this.issued = false;
+            this.issueDate = null;
+            this.returnDate = null;
             return true;
-        }
-    }
-    public final boolean pratestiLeidini(){
-        if(!this.issued) {
+        }else{
             return false;
         }
-        returnDate = returnDate.plusWeeks(1);
-        return true;
     }
 
 
     // Getters
-    public String getAuthor(){return author;}
     public String getName(){return name;}
     public LocalDate getIssueDate(){return issueDate;}
     public LocalDate getReturnDate(){return returnDate;}
     public boolean getIssued(){return issued;}
+    public int getIssueYear(){return issueYear;}
     // Setters
-    public void setAuthor(String author){this.author = author;}
+
     public void setName(String name){this.name = name;}
     public void setIssueDate(LocalDate issueDate){this.issueDate = issueDate;}
     public void setReturnDate(LocalDate returnDate){this.returnDate = returnDate;}
     public void setIssued(boolean issued){this.issued = issued;}
+    public void setIssueYear(int issueYear){this.issueYear = issueYear;}
 }
