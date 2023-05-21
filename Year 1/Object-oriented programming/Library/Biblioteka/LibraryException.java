99921e1fe4704e8df91da70839b7426bb308327c
Dominykas Černovas <113939016+Kurbamit@users.noreply.github.com>
2023-05-21 22:57:56 +0300

    Update LibraryException.java

--- Year 1/Object-oriented programming/Library/Biblioteka/LibraryException.java
+++ Year 1/Object-oriented programming/Library/Biblioteka/LibraryException.java
@@ -1,7 +1,23 @@
 package Biblioteka;
 
+/**
+ * Klasė, skirta išimčiai, kurią galima iššaukti, kai įvyksta klaida bibliotekoje.
+ * Klasė paveldi Exception klasę.
+ * @author Dominykas Černovas.
+ * @version 1.0
+ * @since 1.0
+ */
 public class LibraryException extends Exception{
+    /**
+     * Bibliotekos išimties konstruktorius.
+     * Konstruktorius skirtas sukurti tuščią išimtį.
+     */
     public LibraryException(){}
+    /**
+     * Bibliotekos išimties konstruktorius.
+     * Konstruktorius skirtas sukurti išimtį su pranešimu.
+     * @param message - pranešimas, kuris bus išvestas, kai įvyks klaida.
+     */
     public LibraryException(String message){
         super(message);
     }
