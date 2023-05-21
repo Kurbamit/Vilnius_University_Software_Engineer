9cb46b0f55f992ee689d4e9003ecb3b0b8290278
Dominykas Černovas <113939016+Kurbamit@users.noreply.github.com>
2023-05-21 22:58:13 +0300

    Update NegativeDaysException.java

--- Year 1/Object-oriented programming/Library/Biblioteka/NegativeDaysException.java
+++ Year 1/Object-oriented programming/Library/Biblioteka/NegativeDaysException.java
@@ -1,6 +1,24 @@
 package Biblioteka;
 
+/**
+ * Klasė, skirta išimčiai, kuri atsitinka, kai įvedamas neigiamas dienų skaičius.
+ * Klasė paveldi LibraryException klasę.
+ * @author Dominykas Černovas.
+ * @version 1.0
+ * @since 1.0
+ */
 public class NegativeDaysException extends LibraryException{
+
+    /**
+     * Kintamasis (int tipo).
+     * Dienų skaičius, kuris buvo įvestas
+     */
     public int days;
+    /**
+     * Neigiamų dienų skaičiaus išimties konstruktorius.
+     * Konstruktorius skirtas sukurti išimtį su pranešimu.
+     * @param message - pranešimas, kuris bus išvestas, kai įvyks klaida.
+     * @param days - dienų skaičius, kuris buvo įvestas.
+     */
     public NegativeDaysException(String message, int days){super(message); this.days = days;}
 }
