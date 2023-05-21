23787b1d0980a8311cfceedac899021b7f42de96
Dominykas Černovas <113939016+Kurbamit@users.noreply.github.com>
2023-05-21 22:54:27 +0300

    Update Main.java

--- Year 1/Object-oriented programming/Library/Main.java
+++ Year 1/Object-oriented programming/Library/Main.java
@@ -1,31 +1,20 @@
-package Main;
-import Biblioteka.*;
+import UserInterface.UserInterface;
 
 
+/**
+ * Main klasė, skirta paleisti programą.
+ * @author Dominykas Černovas.
+ * @version 1.0
+ * @since 2023-05-07
+ */
 public class Main {
+    /**
+     * Main metodas, skirtas paleisti programą.
+     * @param args - argumentai, kuriuos galima perduoti programai.
+     */
     public static void main(String[] args) {
-        Knyga K1 = new Knyga("Hamletas", "Sekspyras", 2020);
-        Knyga K2 = new Knyga();
 
-        Zurnalas Z1 = new Zurnalas();
-        Zurnalas Z2 = new Zurnalas("Savaitė", "TV3", 2023);
-
-
-        Knyga K3 = new Knyga("Hamletas", "Sekspyras", 2020);
-        Zurnalas Z3 = new Zurnalas("Savaitė", "TV3", 2023);
-
-
-        //TESTING DEEP CLONING
-        try{
-            Z1 = (Zurnalas) Z2.clone();
-        }catch (CloneNotSupportedException cnse)
-        {
-            System.out.println(cnse);
-        }
-
-        Z1.remejas.setRemejas("PILDYK");
-
-        System.out.println(Z2);
-        System.out.println(Z1);
+        UserInterface ui = new UserInterface();
+        ui.start();
     }
 }
